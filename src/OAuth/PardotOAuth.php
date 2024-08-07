b3cde793bb9c82ee9ca50e58926e085a8c0bb3b7
Andreas Sommer <sommer@belsignum.com>
2024-08-07 21:05:34 +0200

    [TASK] Add domain

--- src/OAuth/PardotOAuth.php
+++ src/OAuth/PardotOAuth.php
@@ -4,8 +4,9 @@ namespace CyberDuck\Pardot\OAuth;
 
 class PardotOAuth
 {
-    private string $authorize_uri = 'https://login.salesforce.com/services/oauth2/authorize';
-    private string $token_uri = 'https://login.salesforce.com/services/oauth2/token';
+    private string $domain = 'https://login.salesforce.com';
+    private string $authorize_uri = '/services/oauth2/authorize';
+    private string $token_uri = '/services/oauth2/token';
 
     private string $client_id;
     private string $client_secret;
@@ -14,14 +15,30 @@ class PardotOAuth
     public function __construct(
         string $client_id,
         string $client_secret,
-        string $redirect_uri
+        string $redirect_uri,
+        string $domain
     )
     {
         $this->client_id = $client_id;
         $this->client_secret = $client_secret;
         $this->redirect_uri = $redirect_uri;
+        $this->domain = $domain;
     }
 
+    public function getAuthorizeUri(): string
+    {
+        return $this->getDomain() . $this->authorize_uri;
+    }
+
+    public function getTokenUri(): string
+    {
+        return $this->getDomain() . $this->token_uri;
+    }
+
+    public function getDomain(): string
+    {
+        return $this->domain;
+    }
 
     public function getAuthorizationUri(): string
     {
@@ -32,14 +49,14 @@ class PardotOAuth
             'scope' => ''
         ];
         $query = http_build_query($data);
-        return sprintf('%s?%s', $this->authorize_uri, $query);
+        return sprintf('%s?%s', $this->getAuthorizeUri(), $query);
     }
 
     public function getAccessToken($authorizationCode): string
     {
         $ch = curl_init();
 
-        curl_setopt($ch, CURLOPT_URL,$this->token_uri);
+        curl_setopt($ch, CURLOPT_URL,$this->getTokenUri());
         curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_POSTFIELDS,
             http_build_query([
--- src/OAuth/RequestOAuth.php
+++ src/OAuth/RequestOAuth.php
@@ -7,6 +7,8 @@ $html = '<form>
 <input type="text" name="client_id"><br>
 <label>client_secret</label>
 <input type="text" name="client_secret"><br>
+<label>Domain</label>
+<input type="text" name="domain"><br>
 <input type="hidden" name="code" value="' . $code . '">
 <input type="submit" value="Submit">
 </form>';
@@ -17,7 +19,8 @@ if(isset($_GET['client_id']) && isset($_GET['client_secret']))
     $oAuth = new \CyberDuck\Pardot\OAuth\PardotOAuth(
         $_GET['client_id'],
         $_GET['client_secret'],
-        sprintf('https://%s%s', $_SERVER['HTTP_HOST'], $_SERVER['REQUEST_URI'])
+        sprintf('https://%s%s', $_SERVER['HTTP_HOST'], $_SERVER['REQUEST_URI']),
+        $_GET['domain']
     );
     $html = '';
 }
